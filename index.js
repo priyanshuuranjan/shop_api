315de0630bde57dc80be12cfaf2c486b1dfeb128
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-08 01:35:48 +0530

    push

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(express.static('images'));
 
 app.use(cors());
 
+// Middleware for logging request URLs
+app.use((req, res, next) => {
+  console.log('Request URL:', req.originalUrl);
+  next();
+});
+
 // Update the path to data.json to point to the "src" folder
 // const apiData = require("../data.json");
 const apiData = require("./data.json")
@@ -19,10 +25,10 @@ app.get("/", (req, res) => {
 
 app.get("/products", (req, res) => {
     res.send(apiData);
-  });
+});
 
 // Start the server
 app.listen(PORT, () => {
-  console.log("im live");
+  console.log("I'm live");
   console.log(`Server is running on port ${PORT}`);
 });
