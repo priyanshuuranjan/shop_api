8636aaf14995a67c02b48291660b13f1ca4cd5d7
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-08 19:09:28 +0530

    push

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+// const express = require("express");
+// const app = express();
+
+// const cors = require("cors");
+// const PORT = process.env.PORT || 3000;
+
+// // Serve static files from the 'images' folder
+// app.use(express.static('images'));
+
+// app.use(cors());
+
+// // Middleware for logging request URLs
+// app.use((req, res, next) => {
+//   console.log('Request URL:', req.originalUrl);
+//   next();
+// });
+
+// // Update the path to data.json to point to the "src" folder
+// // const apiData = require("../data.json");
+// const apiData = require("./data.json")
+
+// app.get("/", (req, res) => {
+//   res.send("hello");
+// });
+
+// app.get("/products", (req, res) => {
+//     res.send(apiData);
+// });
+
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log("I'm live");
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
+
+
+
 const express = require("express");
 const app = express();
 
@@ -16,15 +55,26 @@ app.use((req, res, next) => {
 });
 
 // Update the path to data.json to point to the "src" folder
-// const apiData = require("../data.json");
-const apiData = require("./data.json")
+const apiData = require("./data.json");
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
 app.get("/products", (req, res) => {
-    res.send(apiData);
+  res.send(apiData.products);
+});
+
+// Handle requests for a specific product by ID
+app.get("/products/:id", (req, res) => {
+  const productId = req.params.id;
+  const product = apiData.products.find(p => p.id === productId);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ error: 'Product not found' });
+  }
 });
 
 // Start the server
